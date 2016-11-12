9d10d8aee0d74bebc14c7ed9c57db00f4e583a5f
H-World <370176334@qq.com>
2016-11-12 17:29:34 +0800

    测试
    新建辅助工具类

---
+++ GOUtility/GOUtility/GOUtility.h
@@ -0,0 +1,13 @@
+//
+//  GOUtility.h
+//  GOUtility
+//
+//  Created by 王寒标 on 16/11/12.
+//  Copyright © 2016年 王寒标. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GOUtility : NSObject
+
+@end
---
+++ GOUtility/GOUtility/GOUtility.m
@@ -0,0 +1,13 @@
+//
+//  GOUtility.m
+//  GOUtility
+//
+//  Created by 王寒标 on 16/11/12.
+//  Copyright © 2016年 王寒标. All rights reserved.
+//
+
+#import "GOUtility.h"
+
+@implementation GOUtility
+
+@end
